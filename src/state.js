ec4508ce3da18ee3bba360f60eb071be1121ed2c
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-19 14:47:34 +0200

    Generate TvShows

--- src/state.js
+++ src/state.js
@@ -3,9 +3,11 @@ import axios from "axios"
 
 export const state = new reactive({
   searchMovieBaseApi: "https://api.themoviedb.org/3/search/movie",
+  searchTvShowBaseApi: "https://api.themoviedb.org/3/search/tv",
   apiKey: "95f13a6c93fac8b27d2ad3300a029225",
   searchText: "",
   fetchedMovies: [],
+  fetchedTvShows: [],
   generateMovie() {
     let url = `${this.searchMovieBaseApi}?api_key=${this.apiKey}&query=${this.searchText.toLowerCase()}`;
     axios.get(url)
@@ -17,4 +19,15 @@ export const state = new reactive({
       console.error(error.message);
     })
   },
+  generateTvShows() {
+    let url = `${this.searchTvShowBaseApi}?api_key=${this.apiKey}&query=${this.searchText.toLowerCase()}`;
+    axios.get(url)
+    .then(response => {
+      console.log(response.data.results);
+      this.fetchedTvShows = response.data.results;
+    })
+    .catch(error => {
+      console.error(error.message);
+    })
+  }
 })
